0f51c8ce17570b105ec88fb0fd7c128aa9aa51ca
Anand Gupta <spartango@gmail.com>
2016-02-14 22:17:30 -0500

    merge and gradle fix

--- src/main/java/com/spartango/infra/deploy/TraverseMain.java
+++ src/main/java/com/spartango/infra/deploy/TraverseMain.java
@@ -1,4 +1,4 @@
-package com.spartango.infra.deploy;
+opackage com.spartango.infra.deploy;
 
 import com.spartango.infra.core.OSMGraph;
 import com.spartango.infra.core.OSMIndex;
@@ -42,6 +42,7 @@ public class TraverseMain {
     private static final String DAMAGE_GEOJSON   = PATH + OUTPUT_PATH + "damage.geojson";
 
     private static final long BRIDGE_LIMIT = 5000;
+
     private static GraphDatabaseService graphDb;
 
     public static void main(String[] args) {
