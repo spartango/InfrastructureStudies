ecccaf5b148a66f7e9a779af2e0980285d89f2f9
Anand Gupta <spartango@gmail.com>
2016-03-14 23:40:13 -0700

    more refactoring to more files

--- viz/src/layers.js
+++ viz/src/layers.js
@@ -422,28 +422,6 @@ var loadDamagedPath = function (id) {
         });
 };
 
-var costToHours = function (cost) {
-    return cost / 100000; // meters @ 100kmph,
-};
-
-var addCommas = function (number) {
-    return ("" + number).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
-
-var formatTime = function (hours) {
-    if (hours > 120) {
-        return Math.round(hours / 24) + " days";
-    } else if (hours < 1) {
-        return Math.round(hours * 60) + " minutes";
-    } else {
-        return addCommas(Math.round(hours)) + " hours";
-    }
-};
-
-var formatDistance = function (kilometers) {
-    return kilometers > 1.0 ? addCommas(Math.round(kilometers)) + " km" : Math.round(kilometers * 1000) + " m";
-};
-
 var drawAimPoints = false;
 
 // Legend
