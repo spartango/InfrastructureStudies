6bfc75b09c04419cfff1b3dbd3b22b2e1693f6dc
Anand Gupta <spartango@gmail.com>
2016-04-27 21:19:17 -0400

    fixed the tutorial

--- viz/src/tutorial.js
+++ viz/src/tutorial.js
@@ -12,41 +12,41 @@ var startTutorial = function () {
             position: 'bottom',
             before: function () {
                 hideLegend();
-                hideMapLayer('sources');
-                hideMapLayer('sinks');
+                hideLayer('suppliers');
+                hideLayer('consumers');
                 hideMapLayer('paths');
                 hideMapLayer('targets');
                 clearAnimation();
             }
         },
         {
-            intro: `<h5><i class="fa fa-` + typeIcons['supplier']
+            intro: `<h5><i class="fa fa-` + dataLayers.suppliers.icon
             + `"></i> | Suppliers</h5>`
             + `<p> The blue markers identify suppliers of resources, such as food, fuel, and parts. `
             + `Trains load supplies at and depart from stations near these sites. `
             + `Click on any supplier for more information about it.</p>`,
             position: 'bottom',
             before: function () {
-                hideMapLayer('sinks');
-                showSources();
+                hideMapLayer('consumers');
+                showLayer('suppliers');
             }
         },
         {
-            intro: `<h5><i class="fa fa-` + typeIcons['consumer']
+            intro: `<h5><i class="fa fa-` + dataLayers.consumers.icon
             + `"></i> | Consumers</h5><p>The purple markers indicate consumers of those resources, `
             + "such as maintenance facilities, populations, or military units. "
             + "Trains carrying supplies are offloaded at stations near these locations. "
             + `Click on any consumer for more information about it.</p>`,
             position: 'bottom',
             before: function () {
-                hideMapLayer('sources');
+                hideLayer('suppliers');
                 hideMapLayer('paths');
                 clearAnimation();
-                showSinks()
+                showLayer('consumers');
             }
         },
         {
-            intro: `<h5><i class="fa fa-` + typeIcons['flow']
+            intro: `<h5><i class="fa fa-exchange`
             + `"></i> | Flows</h5><p>The black paths represent the best railroad routes connecting suppliers to consumers, `
             + "with small dots indicating flow direction. "
             + "Each path is optimized for speed, minimizing the costs associated with traveling across the terrain. </p>",
@@ -54,8 +54,12 @@ var startTutorial = function () {
             before: function () {
                 hideMapLayer('targets')
                     .then(hideLegend)
-                    .then(showSources)
-                    .then(showSinks)
+                    .then(function () {
+                        return showLayer('suppliers');
+                    })
+                    .then(function () {
+                        return showLayer('consumers');
+                    })
                     .then(showPaths)
                     .then(function () {
                         showAnimation('baseline');
@@ -63,23 +67,23 @@ var startTutorial = function () {
             }
         },
         {
-            intro: `<h5><i class="fa fa-` + typeIcons['target']
+            intro: `<h5><i class="fa fa-crosshairs`
             + `"></i> | Resilience</h5><p>The highlighted segments are vulnerable bridges along the rail routes. `
             + "Their relative vulnerability is indicated by color from yellow (low) to red (high) "
             + "and is determined by simulating damage to each bridge. "
             + `Click on any bridge for more information about it. </p>`,
             position: 'bottom',
             before: function () {
-                hideClusterLayer('sams')
+                hideLayer('SAMs')
                     .then(function () {
-                        return hideMapLayer('rings');
+                        return hideLayer('rangeRings');
                     })
                     .then(showTargets)
                     .then(clearAnimation);
             }
         },
         {
-            intro: `<h5><i class="fa fa-` + typeIcons['SAM']
+            intro: `<h5><i class="fa fa-` + dataLayers.SAMs.icon
             + `"></i> | Defense</h5><p>`
             + `The green markers indicate Surface-to-Air Missile (SAM) sites, radars, and airbases`
             + " protecting the rail network from aerial attack. Selecting a bridge will suggest the nearest "
@@ -87,7 +91,10 @@ var startTutorial = function () {
             + `Click on any site for more information about it.</p>`,
             position: 'bottom',
             before: function () {
-                showSAMThreats();
+                showLayer('SAMs')
+                    .then(function () {
+                        return showLayer('rangeRings');
+                    })
             }
         }
     ];
@@ -111,15 +118,15 @@ var startTutorial = function () {
         }
     }).oncomplete(function () {
         window.location.hash = "#";
-        hideClusterLayer('sams')
+        hideLayer('SAMs')
             .then(function () {
-                return hideMapLayer('rings');
+                return hideLayer('rangeRings');
             }).then(showDefaultLayers);
     }).onexit(function () {
         window.location.hash = "#";
-        hideClusterLayer('sams')
+        hideLayer('SAMs')
             .then(function () {
-                return hideMapLayer('rings');
+                return hideLayer('rangeRings');
             }).then(showDefaultLayers);
     });
     intro.start();
